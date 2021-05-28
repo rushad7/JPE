3159e98e4ccbed040f35e8d9e086732f7d755f38
Rushad Mehta <rushadmehta16@gmail.com>
2021-05-28 14:16:07 +0530

    Added timeInstant attribute to track global time.Modified runParallel() and runSequential() methods.

--- src/jpe/Environment.java
+++ src/jpe/Environment.java
@@ -1,62 +1,41 @@
 package jpe;
 
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.stream.IntStream;
 
 
 public class Environment {
 
+    private static double timeInstant;
     protected ArrayList<Entity> entities;
-
     protected static double timeStep;
-    protected final double[] time;
 
-
-    public Environment(int simTimeStop, double timeStep) {
+    public Environment(double timeStep) {
+        timeInstant = 0;
         entities = new ArrayList<>();
-
         Environment.timeStep = timeStep;
-        this.time = IntStream.range(0, simTimeStop).mapToDouble(i -> (double) i).toArray();
     }
 
     public void addEntity(Entity entity) {
         entities.add(entity);
     }
 
-    private void runParallel(Actions actions) {
-
-        for (double timeInstant : time) {
-
-            actions.formulate();
-
-            System.out.println("--------------------------");
-            System.out.println("[ITERATION " + (int) timeInstant + "]");
-            System.out.println("--------------------------");
-
-            for (Entity entity : entities) {
-                System.out.println("Entity: " + entity.name);
-                HashMap<String, double[]> states = entity.inspectStates();
-                states.forEach((String state, double[] vector) -> System.out.println(state + " " + Arrays.toString(vector)));
-                System.out.println("--------------------------");
-            }
-
-            System.out.println("\n##########################\n");
-        }
+    public static double getTimeInstant() {
+        return timeInstant;
     }
 
-    private void runSequential() {
-
+    public static void incrementTimeInstant() {
+        timeInstant += 1;
     }
 
-    public void run(Actions actions) {
+    public void runSequential(Execution execution) {
+        execution.define();
+    }
 
-        if (actions.execution == Execution.Parallel) {
-            runParallel(actions);
-        } else if (actions.execution == Execution.Sequential) {
-            runSequential();
+    public void runParallel(Execution execution, double repeat) {
+        for (double time = Environment.getTimeInstant(); time < (repeat + Environment.getTimeInstant()); time += Environment.timeStep) {
+            System.out.println("[TIME " + time + "]");
+            execution.define();
         }
-
+        Environment.incrementTimeInstant();
     }
 }
