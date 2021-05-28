ce1eeda2c07a0977f9daefe2005f7df3390e3c69
Rushad Mehta <rushadmehta16@gmail.com>
2021-05-28 14:22:25 +0530

    Created Parameter class to define parameters to actions and entities.

--- src/jpe/Parameter.java
+++ src/jpe/Parameter.java
@@ -1,2 +1,31 @@
-package jpe;public class Parameter {
+package jpe;
+
+
+public class Parameter {
+
+    protected final Action action;
+    protected double[] acceleration;
+    protected double[] force;
+
+    public Parameter(Action action) {
+        this.action = action;
+    }
+
+    public void defineAcceleration(double[] acceleration) throws IncorrectParameterError {
+        if (this.action == Action.ACCELERATION) {
+            this.acceleration = acceleration;
+        } else {
+            throw new IncorrectParameterError("Did not expect action 'ACCELERATE' here.");
+        }
+    }
+
+    public void defineForce(double[] force) throws IncorrectParameterError {
+        if (this.action == Action.FORCE) {
+            this.force = force;
+        } else {
+            throw new IncorrectParameterError("Did not expect action 'FORCE' here.");
+        }
+    }
+
 }
+
