b044f2534dd267f150a1e1a2fa53c5bbf60f46bd
Rushad Mehta <rushadmehta16@gmail.com>
2021-05-28 14:23:11 +0530

    Modified parameter type

--- src/jpe/Physics.java
+++ src/jpe/Physics.java
@@ -6,9 +6,9 @@ public class Physics {
     public Physics() {
     }
 
-    public static void accelerate(Entity entity, double[] acceleration) {
-        double vx = entity.getVelocity()[0] + acceleration[0] * Environment.timeStep;
-        double vy = entity.getVelocity()[1] + acceleration[1] * Environment.timeStep;
+    public static void accelerate(Entity entity, Parameter parameter) {
+        double vx = entity.getVelocity()[0] + parameter.acceleration[0] * Environment.timeStep;
+        double vy = entity.getVelocity()[1] + parameter.acceleration[1] * Environment.timeStep;
         entity.setVelocity(new double[]{vx, vy});
 
         double px = entity.getPosition()[0] + vx * Environment.timeStep;
@@ -16,9 +16,9 @@ public class Physics {
         entity.setPosition(new double[]{px, py});
     }
 
-    public static void applyForce(Entity entity, double[] force) {
-        double vx = entity.getVelocity()[0] + ((force[0] / entity.getMass()) * Environment.timeStep);
-        double vy = entity.getVelocity()[1] + ((force[1] / entity.getMass()) * Environment.timeStep);
+    public static void applyForce(Entity entity, Parameter parameter) {
+        double vx = entity.getVelocity()[0] + ((parameter.force[0] / entity.getMass()) * Environment.timeStep);
+        double vy = entity.getVelocity()[1] + ((parameter.force[1] / entity.getMass()) * Environment.timeStep);
         entity.setVelocity(new double[]{vx, vy});
 
         double px = entity.getPosition()[0] + vx * Environment.timeStep;
