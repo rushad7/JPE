6602570aabc56fc449e274897ea57a6087c9d3b6
Rushad Mehta <rushadmehta16@gmail.com>
2021-05-30 19:31:43 +0530

    Modified model to account for frictional forces in 2 dimensions.

--- src/jpe/Physics.java
+++ src/jpe/Physics.java
@@ -7,8 +7,10 @@ public class Physics {
     }
 
     public static void accelerate(Entity entity, Parameter parameter) {
-        double vx = entity.getVelocity()[0] + parameter.acceleration[0] * Environment.timeStep;
-        double vy = entity.getVelocity()[1] + parameter.acceleration[1] * Environment.timeStep;
+        double vx = entity.getVelocity()[0] +
+                (parameter.acceleration[0] - parameter.coefficientOfFriction[0] * Environment.ACC_GRAV * Math.cos(Environment.incline)) * Environment.timeStep;
+        double vy = entity.getVelocity()[1] +
+                (parameter.acceleration[1] - parameter.coefficientOfFriction[1] * Environment.ACC_GRAV * Math.cos(Environment.incline)) * Environment.timeStep;
         entity.setVelocity(new double[]{vx, vy});
 
         double px = entity.getPosition()[0] + vx * Environment.timeStep;
@@ -17,8 +19,12 @@ public class Physics {
     }
 
     public static void applyForce(Entity entity, Parameter parameter) {
-        double vx = entity.getVelocity()[0] + ((parameter.force[0] / entity.getMass()) * Environment.timeStep);
-        double vy = entity.getVelocity()[1] + ((parameter.force[1] / entity.getMass()) * Environment.timeStep);
+        double vx = entity.getVelocity()[0] +
+                (((parameter.force[0] / entity.getMass())
+                        - parameter.coefficientOfFriction[0] * Environment.ACC_GRAV * Math.cos(Environment.incline)) * Environment.timeStep);
+        double vy = entity.getVelocity()[1] +
+                (((parameter.force[1] / entity.getMass())
+                        - parameter.coefficientOfFriction[1] * Environment.ACC_GRAV * Math.cos(Environment.incline)) * Environment.timeStep);
         entity.setVelocity(new double[]{vx, vy});
 
         double px = entity.getPosition()[0] + vx * Environment.timeStep;
